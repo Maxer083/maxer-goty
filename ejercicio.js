697a7481bc8efd4a8a0b691fe055e98b71307d00
Alumno <alumno@et26.local>
2026-03-30 16:19:37 -0300

    trabajo 1

--- ejercicio.js
+++
@@ -1,6 +0,0 @@
-var meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
-console.log(meses)
-
-//fin de un codigo sigue el otro para probar en paginas
-
-var valores = [true, 5, false, "hola", "adios",2];
